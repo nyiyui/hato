0d46308f42202643c56a7b5a0c4b6bb87365954b
nyiyui <+@nyiyui.ca>
2023-05-02 11:32:28 -0400

    weighting

--- breakbeam/breakbeam.ino
+++ breakbeam/breakbeam.ino
@@ -1,6 +1,3 @@
-unsigned long logInterval = 1000;
-bool logData = true;
-
 void setup() {
   Serial.begin(9600);
   pinMode(7, INPUT_PULLUP);
@@ -10,22 +7,61 @@ void setup() {
 
 void loop() {
   handleSLCP();
-  if (logData)
-    doLogging();
+  doLogging();
+}
+
+#define CLAMP_LIMIT 300
+#define THRESHOLD 290
+
+long clamp(long a) {
+  if (a < 0) return 0;
+  if (a > CLAMP_LIMIT) return CLAMP_LIMIT;
+  return a;
+}
+
+long update(bool current, long weighted, unsigned long elapsed) {
+  if (current) weighted += (long) elapsed;
+  if (!current) weighted -= (long) elapsed;
+  return clamp(weighted);
 }
 
 void doLogging() {
+  static long aWeighted = 0;
+  static long bWeighted = 0;
+  static bool aOld = false;
+  static bool bOld = false;
   static unsigned long prev = millis();
   unsigned long now = millis();
-  if (now - prev <= logInterval)
-    return;
+  unsigned long elapsed = now - prev;
+  //          ( types? wat r those )
+  //          |/
+  // ¯\_(ツ)_/¯
+  bool aRaw = digitalRead(7) == HIGH;
+  bool bRaw = digitalRead(10) == HIGH;
+  aWeighted = update(aRaw, aWeighted, elapsed);
+  bWeighted = update(bRaw, bWeighted, elapsed);
+  /*
+  Serial.print(" aWeighted ");
+  Serial.print(aWeighted);
+  Serial.print(" bWeighted ");
+  Serial.print(bWeighted);
+  Serial.print(" elapsed ");
+  Serial.print(elapsed);
+  Serial.println();
+  */
+  bool aCur = aWeighted > THRESHOLD;
+  bool bCur = bWeighted > THRESHOLD;
+  if (aCur != aOld || bCur != bOld) {
+    Serial.print(" DA");
+    Serial.print(aCur);
+    Serial.print("B");
+    Serial.print(bCur);
+    Serial.print("T");
+    Serial.println(now);
+  }
+  aOld = aCur;
+  bOld = bCur;
   prev = now;
-  Serial.print(" DA");
-  Serial.print(digitalRead(7));
-  Serial.print("B");
-  Serial.print(digitalRead(10));
-  Serial.print("T");
-  Serial.println(now);
 }
 
 void handleSLCP() {
@@ -34,44 +70,11 @@ void handleSLCP() {
     return;
   int kind = Serial.read();
   if (kind == 'I') {
-    Serial.println(" Isoyuu-breakbeam itsybitsy0/0");
+    Serial.println(" Isoyuu-breakbeam/itsybitsy0/0");
     int eol = Serial.read();
     if (eol != '\n') {
       Serial.println(" Eexpected eol");
     }
-  } else if (kind == 'S') {
-    size_t read = Serial.readBytesUntil('\n', buffer, 10);
-    // [1] op - only 'i' for interval
-    // [4] val - for 'i', 4 digits on decimal for log interval ms
-    if (read != 5) {
-      Serial.print(" Einvalid length ");
-      Serial.println(read);
-      return;
-    }
-    if (buffer[0] != 'i') {
-      Serial.print(" Einvalid op ");
-      Serial.println(buffer[0]);
-      return;
-    }
-    int val = atoi(buffer + 1);
-    logInterval = (unsigned long)val;
-    Serial.print(" Olog interval changed to ");
-    Serial.println(logInterval);
-  } else if (kind == 'L') {
-    size_t read = Serial.readBytesUntil('\n', buffer, 10);
-    if (read != 1) {
-      Serial.print(" Einvalid length ");
-      Serial.println(read);
-      return;
-    }
-    if (buffer[0] != '0' && buffer[0] != '1') {
-      Serial.print(" Einvalid op ");
-      Serial.println(buffer[0]);
-      return;
-    }
-    logData = buffer[0] == '1';
-    Serial.print(" Ologging changed to ");
-    Serial.println(logData);
   } else {
     Serial.print(" Eunknown kind ");
     Serial.println(kind);
--- breakbeam/sync.sh
+++ breakbeam/sync.sh
@@ -2,8 +2,9 @@
 
 set -eux
 
+port='/dev/ttyACM0'
 fqbn='adafruit:samd:adafruit_itsybitsy_m0'
 arduino-cli compile --fqbn "$fqbn"
-arduino-cli upload -p /dev/ttyACM0 --fqbn "$fqbn"
-sleep 2
-picocom /dev/ttyACM0
+arduino-cli upload -p "$port" --fqbn "$fqbn"
+#sleep 2
+#picocom "$port"
